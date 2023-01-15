a732a7767544c407d47a4ee456bfa79f86eeeb09
Alexander Lobachev <ss_di@mail.ru>
2023-01-15 17:23:19 +0300

    добавлено применение правил при запуске команд

--- bin/fw_off.sh
+++ bin/fw_off.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 sed 's/^CONFIG_FW=.*/CONFIG_FW=no/' -i /etc/net/ifaces/default/options
+systemctl restart network
--- bin/fw_on.sh
+++ bin/fw_on.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 sed 's/^CONFIG_FW=.*/CONFIG_FW=yes/' -i /etc/net/ifaces/default/options
+systemctl restart network
